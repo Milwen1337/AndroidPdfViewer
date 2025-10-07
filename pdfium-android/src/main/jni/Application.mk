6f97196beaf04bdb70142012b5594ab10cb5dc81
Martin Marič <mmaricdev@gmail.com>
2025-10-07 18:50:42 +0200

    Trying to fix ndk/Paging issue

--- pdfium-android/src/main/jni/Application.mk
+++ pdfium-android/src/main/jni/Application.mk
@@ -8,3 +8,6 @@ APP_ABI :=  armeabi-v7a \
             arm64-v8a \
             x86 \
             x86_64
+			
+# 16 KB page size (both common and max)
+APP_LDFLAGS += -Wl,-z,common-page-size=16384 -Wl,-z,max-page-size=16384
